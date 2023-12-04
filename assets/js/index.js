f35f811d4891a0ca3c7627998957a8633b7348c7
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2023-12-04 14:50:18 +0100

    addevent listener con eventcurrenttarget

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,21 @@
 /* fixata animazione carosello buggata per overflow-visible*/
-let carousels = document.querySelectorAll(".carousel");
 
+let buttons = document.querySelectorAll(".carousel button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    let inner = event.currentTarget.closest(".carousel").querySelector(".carousel-inner");
+    inner.classList.add("overflow-hidden");
+    inner.classList.remove("overflow-visible");
+    setTimeout(function () {
+      inner.classList.add("overflow-visible");
+      inner.classList.remove("overflow-hidden");
+    }, 650);
+  });
+});
+
+//let carousels = document.querySelectorAll(".carousel");
+/*
 carousels.forEach((carousel) => {
   let inner = carousel.querySelector(".carousel-inner");
   let buttons = carousel.querySelectorAll("button");
@@ -15,7 +30,7 @@ carousels.forEach((carousel) => {
     });
   });
 });
-
+*/
 /* test sul singolo carousel
       let innerCar = document.querySelector("#carouselExample3 .carousel-inner");
       let button = document.querySelector("#carouselExample3 button");
